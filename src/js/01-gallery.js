945e990e928f8925f48c8bd368dfbf4b3799615f
Timur4ick <79656475+Timur4ick@users.noreply.github.com>
2021-12-20 20:11:53 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallary = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const cardsGallery = imgCards(galleryItems);
+
+function imgCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(' ');
+}
+gallary.insertAdjacentHTML('beforeend', cardsGallery);
+
+gallary.addEventListener('click', clickBtn);
+
+function clickBtn(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+let lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+lightbox.show();
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const idPlayer = new Vimeo.Player('vimeo-player');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', () => {
+  player.getCurrentTime().then(sec => {
+    console.log(sec);
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
+  });
+});
+
+const timeToStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+console.log(timeToStart);
+player.setCurrentTime(timeToStart);
